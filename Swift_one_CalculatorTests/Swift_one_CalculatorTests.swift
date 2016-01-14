c9aaa5ddc648f969b9f5bfd305bcd86c27483d0c
Wei TingTao <s952865@go.thu.edu.tw>
2016-01-14 11:39:56 -0600

    Initial Commit

---
+++ Swift_one_CalculatorTests/Swift_one_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_one_CalculatorTests.swift
+//  Swift_one_CalculatorTests
+//
+//  Created by Wei TingTao on 1/14/16.
+//  Copyright © 2016 weitingtao. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_one_Calculator
+
+class Swift_one_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
